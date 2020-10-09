0e5c308af9f396158067629fd427f70deea4da61
Diego <diegogeber@gmail.com>
2020-10-08 21:03:36 -0300

    v2 - inclui inicio e condicoes da query

--- src/Test.java
+++ src/Test.java
@@ -8,15 +8,36 @@ import java.io.OutputStreamWriter;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
-		OutputStream os = new FileOutputStream("C:/saida.txt");
+		OutputStream os = new FileOutputStream("C:/output.txt");
 		OutputStreamWriter osw = new OutputStreamWriter(os);
 		BufferedWriter bw = new BufferedWriter(osw);
-		String linha;
-		BufferedReader br = new BufferedReader(new FileReader("C:/amostra.txt"));
-		while ((linha = br.readLine()) != null) {
-		     System.out.println(linha);
-		     bw.write("'" + linha + "',");
-		     bw.newLine();
+		
+		BufferedReader br = new BufferedReader(new FileReader("C:/input.txt"));
+		String line;
+		int elements = 0;
+		bw.write("select * from funcionario f inner join usuario u on u.id_func=f.id where u.cod_usu in ( ");
+		while ((line = br.readLine()) != null) {
+			if(elements == 0){
+				bw.newLine();
+				bw.write("'" + line + "',");
+				bw.newLine();
+				elements++;
+				continue;
+			}
+			
+			if(elements != 1 && elements != 999){
+				bw.write("'" + line + "',");
+				bw.newLine();
+			}else if(elements == 1){
+				bw.write("or u.cod_usu in ('" + line + "',");
+				bw.newLine();
+	    	}else{
+	    		bw.write("'" + line + "')");
+				bw.newLine();
+				elements = 1;
+	    		continue;
+		    }	 
+			elements++;
 		}
 		bw.close();
 	    br.close();
